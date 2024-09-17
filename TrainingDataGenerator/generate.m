0e11dd772f9e6e584a0c704011460c05d1b0b0c5
xliu <xun.liu.1991@gmail.com>
2024-09-17 16:33:21 -0500

    demo done

--- TrainingDataGenerator/generate.m
+++ TrainingDataGenerator/generate.m
@@ -1,9 +1,23 @@
 % generate 100 images with labels, 80 train 20 validation.
 
+folders = {
+    './data', ...
+    './data/images', ...
+    './data/images/train', ...
+    './data/images/val', ...
+    './data/labels', ...
+    './data/labels/train', ...
+    './data/labels/val'
+};
+
+for i = 1:length(folders)
+    mkdir(folders{i});
+end
+
 for i=1:80
     createOneSample(5, 'data/images/train', 'data/labels/train', ['trainImg' num2str(i)]);
 end
 
-for i=1:20
+for i=1:5
     createOneSample(5, 'data/images/val', 'data/labels/val', ['valImg' num2str(i)]);
 end
