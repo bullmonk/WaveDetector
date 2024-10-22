d75ec16216b2faa160a260fa4b2153ee159daa93
xliu <xun.liu.1991@gmail.com>
2024-10-22 12:48:18 -0500

    finished augmentation

--- TrainingDataGenerator/augmentation/augmentation.m
+++ TrainingDataGenerator/augmentation/augmentation.m
@@ -6,7 +6,7 @@ catname = @(i) fullfile(files(i).folder, files(i).name);
 fns = arrayfun(catname, 1:size(files, 1), 'UniformOutput',false);
 
 % target file names.
-augname = @(i) fullfile(files(i).folder, "augmentation", ['aug_' files(i).name]);
+augname = @(i) fullfile(files(i).folder, ['aug_' files(i).name]);
 tns = arrayfun(augname, 1:size(files, 1), 'UniformOutput', false);
 
 % add flip augmentation.
@@ -15,7 +15,7 @@ arrayfun(flipx, 1:size(files, 1));
 
 % ==== create labels ====
 txts = recursiveDir('labels', 'FileType', '.txt');
-catname = @(i) fullfile(txts(i).folder, "augmentation", ['aug_' txts(i).name]);
+catname = @(i) fullfile(txts(i).folder, ['aug_' txts(i).name]);
 tts = arrayfun(catname, 1:size(txts, 1), 'UniformOutput',false);
 T = table();
 creatempty = @(i) writetable(T, tts{i}, "WriteVariableNames", false);
